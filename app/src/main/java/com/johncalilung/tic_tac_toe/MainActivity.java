23a802c62ff8b73de0283a73812f3e3ba3bcd08d
j-cali <johnkcalilung@gmail.com>
2016-12-17 14:21:32 -0800

    Add radio group
    Added radio group for choosing 'X' or 'O' for player choice.

--- app/src/main/java/com/johncalilung/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/johncalilung/tic_tac_toe/MainActivity.java
@@ -1,11 +1,15 @@
 package com.johncalilung.tic_tac_toe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Member Variables
+    private char human_player_mark;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +21,8 @@ public class MainActivity extends AppCompatActivity {
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent i = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(i);
             }
         });
     }
