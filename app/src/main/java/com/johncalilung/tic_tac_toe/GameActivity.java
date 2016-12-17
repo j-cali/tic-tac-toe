249e67a1607b1f21155fb9a0d880da9f39c41676
j-cali <johnkcalilung@gmail.com>
2016-12-17 14:43:41 -0800

    Pass data between activities
    Passed player mark choice from MainActivity to GameActivity

--- app/src/main/java/com/johncalilung/tic_tac_toe/GameActivity.java
+++ app/src/main/java/com/johncalilung/tic_tac_toe/GameActivity.java
@@ -2,12 +2,21 @@ package com.johncalilung.tic_tac_toe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
+
+    // Member Variables
+    private char mHumanPlayerMark;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        mHumanPlayerMark = getIntent().getCharExtra("HUMAN_PLAYER_MARK", 'X');
+
+        TextView tempTextView = (TextView) findViewById(R.id.temp_textview);
+        tempTextView.append("" + mHumanPlayerMark);
     }
 }
--- app/src/main/java/com/johncalilung/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/johncalilung/tic_tac_toe/MainActivity.java
@@ -5,25 +5,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
     // Member Variables
-    private char human_player_mark;
+    private char mHumanPlayerMark;
+    private RadioButton mMarkedButton;
+    private Button mPlayButton;
+    private RadioGroup mRadioGroupMark;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button playButton = (Button) findViewById(R.id.play_button);
+        mPlayButton = (Button) findViewById(R.id.play_button);
+        mRadioGroupMark = (RadioGroup) findViewById(R.id.radio_mark_choice);
 
-        playButton.setOnClickListener(new View.OnClickListener() {
+
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this, GameActivity.class);
+                i.putExtra("HUMAN_PLAYER_MARK", getHumanPlayerMark());
                 startActivity(i);
             }
         });
+
+        mRadioGroupMark.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                mMarkedButton = (RadioButton) findViewById(checkedId);
+                mHumanPlayerMark = mMarkedButton.getText().charAt(0);
+            }
+        });
+    }
+
+    private char getHumanPlayerMark() {
+        return mHumanPlayerMark;
     }
 }
